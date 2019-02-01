536ab8cdf02e62eba017cf2fdbf0a0c939054462
rajaghan <rajasekhar.ghanta@gmail.com>
2019-02-01 11:31:51 +0530

    same line edit

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -3,6 +3,6 @@ public class MainClass {
     public static void main(String[] args){
 
         System.out.println("This is develop branch");
-        //master
+        //changed this line in develop
     }
 }
